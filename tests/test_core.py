fcb075644231ca0d817cb30145aa9adc37a07c76
alexpmr <136476877+alexpmr@users.noreply.github.com>
2026-09-23 18:51:40 -0300

    test: update version parsing fixture for v0.2.7

--- tests/test_core.py
+++ tests/test_core.py
@@ -141,7 +141,7 @@ def test_aprs_passcode():
 
 
 def test_version_tuple():
-    assert version_tuple("v0.2.6") == (0, 2, 6)
+    assert version_tuple("v0.2.7") == (0, 2, 7)
     assert version_tuple("0.2.10") > version_tuple("0.2.9")
     assert version_tuple("v1.0.0") > version_tuple("0.9.99")
 
