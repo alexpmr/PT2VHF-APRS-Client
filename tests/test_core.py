88b1ef12e58902680fa65b97df0f2f9b0f31c154
alexpmr <136476877+alexpmr@users.noreply.github.com>
2026-09-24 00:43:24 -0300

    test: update version fixture for v0.3.0

--- tests/test_core.py
+++ tests/test_core.py
@@ -147,7 +147,7 @@ def test_aprs_passcode():
 
 
 def test_version_tuple():
-    assert version_tuple("v0.2.9") == (0, 2, 9)
+    assert version_tuple("v0.3.0") == (0, 3, 0)
     assert version_tuple("0.2.10") > version_tuple("0.2.9")
     assert version_tuple("v1.0.0") > version_tuple("0.9.99")
 
